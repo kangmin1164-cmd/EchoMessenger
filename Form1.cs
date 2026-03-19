eb53bf0a2b41d836d3d3751ded97ae410889efdb
강민규 <@DESKTOP-7A2RC6P>
2026-03-19 10:50:17 +0900

    과제 1 코딩과 테스트 완료 ListBox 중앙에 TextBox를 배치함 전송 버튼을 누르면 TextBox에 입력한 내용이 ListBox로 들어가 표시되고 TextBox의 내용은 깨끗하게 지워짐.

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Windows.Forms;
+
 namespace EchoMessenger
 {
     public partial class Form1 : Form
@@ -7,9 +10,14 @@ namespace EchoMessenger
             InitializeComponent();
         }
 
+        // 전송 버튼 클릭 이벤트
         private void btnSend_Click(object sender, EventArgs e)
         {
+            // [과제 1] 리스트박스에 텍스트 추가
+            lstChat.Items.Add(txtInput.Text);
 
+            // [과제 1] 입력창 비우기
+            txtInput.Clear();
         }
     }
 }
