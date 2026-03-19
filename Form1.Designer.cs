e447d3cd27308e2d84f8d2ad15e4441363776323
강민규 <@DESKTOP-7A2RC6P>
2026-03-19 10:45:30 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace EchoMessenger
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnSend = new Button();
+            lstChat = new ListBox();
+            txtInput = new TextBox();
+            lblTitle = new Label();
+            SuspendLayout();
+            // 
+            // btnSend
+            // 
+            btnSend.BackColor = Color.FromArgb(0, 192, 0);
+            btnSend.Font = new Font("한컴 고딕", 10F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btnSend.Location = new Point(589, 394);
+            btnSend.Name = "btnSend";
+            btnSend.Size = new Size(93, 46);
+            btnSend.TabIndex = 0;
+            btnSend.Text = "입력";
+            btnSend.UseVisualStyleBackColor = false;
+            btnSend.Click += btnSend_Click;
+            // 
+            // lstChat
+            // 
+            lstChat.Font = new Font("한컴 고딕", 9F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            lstChat.FormattingEnabled = true;
+            lstChat.Location = new Point(119, 74);
+            lstChat.Name = "lstChat";
+            lstChat.Size = new Size(563, 303);
+            lstChat.TabIndex = 1;
+            // 
+            // txtInput
+            // 
+            txtInput.Font = new Font("한컴 고딕", 10F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            txtInput.Location = new Point(119, 401);
+            txtInput.Name = "txtInput";
+            txtInput.Size = new Size(464, 33);
+            txtInput.TabIndex = 2;
+            // 
+            // lblTitle
+            // 
+            lblTitle.AutoSize = true;
+            lblTitle.Font = new Font("Times New Roman", 20F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            lblTitle.ForeColor = Color.FromArgb(128, 128, 255);
+            lblTitle.Location = new Point(119, 9);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new Size(297, 46);
+            lblTitle.TabIndex = 3;
+            lblTitle.Text = "Echo Messenger";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 224, 192);
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblTitle);
+            Controls.Add(txtInput);
+            Controls.Add(lstChat);
+            Controls.Add(btnSend);
+            ForeColor = SystemColors.ControlText;
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnSend;
+        private ListBox lstChat;
+        private TextBox txtInput;
+        private Label lblTitle;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,15 @@
+namespace EchoMessenger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
